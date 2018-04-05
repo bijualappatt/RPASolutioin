3da4142347bdffdcf1c636a8d100a630780b1739
Biju Alappatt <bijusalapatt@gmail.com>
2018-04-05 20:33:30 +0530

    File download feature added

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print ('Hello')
+import requests
+import os
+
+file_url = 'https://tomcat.apache.org/tomcat-7.0-doc/appdev/sample/sample.war'
+
+os.chdir('./dnld')
+
+r = requests.get(file_url)
+
+with open("sample.war", "wb") as code:
+
+    code.write(r.content)
